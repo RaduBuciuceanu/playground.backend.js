7b500301707b06726fdced2845f5285311618e7a
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-12-10 22:50:22 +0200

    [feature/34] Use lodash in routes (#36)

--- src/presentation/routes/get-records.js
+++ src/presentation/routes/get-records.js
@@ -1,9 +1,10 @@
+import { get } from 'lodash'
 import { controllers } from 'presentation/controllers'
 import { handleFailure } from 'presentation/routes/handlers/handle-failure'
 import { handleSuccess } from 'presentation/routes/handlers/handle-success'
 
 export const getRecords = (request, response) => {
-  const parameter = request.body?.parameter?.ids || []
+  const parameter = get(request, 'body.parameter.ids', [])
   const created = controllers.getRecords(parameter)
 
   created.either(handleFailure(response), handleSuccess(response))
