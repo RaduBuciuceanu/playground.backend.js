479c736ba7c2a5820ca04ff1a5d2b0a4e556418e
radu <radu.buciuceanu@gmail.com>
2021-12-10 22:49:47 +0200

    [feature/34] Use lodash in routes

--- src/presentation/routes/create-record.js
+++ src/presentation/routes/create-record.js
@@ -1,9 +1,10 @@
+import { get } from 'lodash'
 import { controllers } from 'presentation/controllers'
 import { handleFailure } from 'presentation/routes/handlers/handle-failure'
 import { handleSuccess } from 'presentation/routes/handlers/handle-success'
 
 export const createRecord = (request, response) => {
-  const input = request.body?.record
+  const input = get(request, 'body.input', undefined)
   const created = controllers.createRecord(input)
 
   created.either(handleFailure(response), handleSuccess(response))
