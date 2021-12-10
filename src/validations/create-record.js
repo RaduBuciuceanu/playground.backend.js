ca9c8c5ccf93be6f5696d0153bf0e385bcc0e361
radu <radu.buciuceanu@gmail.com>
2021-12-10 23:34:24 +0200

    [feature/39] Configure get validations

--- src/validations/create-record.js
+++ src/validations/create-record.js
@@ -1,11 +1,12 @@
 import * as yup from 'yup'
 import { Flow } from 'domain'
+import { validate } from 'validations/core/validate'
 
-const schema = yup.object().shape({ record: yup.string().required('required') })
-
-const validate = Flow.tryCatch(input => schema.validateSync(input, { abortEarly: false }))
+const schema = yup.object().shape({
+  record: yup.string().required('required')
+})
 
 export const createRecord = () => Flow.ask()
   .map(context => context.input)
-  .map(validate)
+  .map(validate(schema))
   .chain(Flow.lift)
