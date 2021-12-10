23cd813493d74fc646f11550402b7362e8e71ecc
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-12-10 23:28:55 +0200

    [feature/37] Configure creation validations (#38)
    * [feature/37] Reference yup package
    
    * [feature/37] Add try catch to flow
    
    * [feature/37] Validate create record

--- src/validations/create-record.js
+++ src/validations/create-record.js
@@ -1,5 +1,11 @@
+import * as yup from 'yup'
 import { Flow } from 'domain'
+
+const schema = yup.object().shape({ record: yup.string().required('required') })
+
+const validate = Flow.tryCatch(input => schema.validateSync(input, { abortEarly: false }))
 
 export const createRecord = () => Flow.ask()
   .map(context => context.input)
-  .map(input => console.log(`Validating the input...`, input))
+  .map(validate)
+  .chain(Flow.lift)
