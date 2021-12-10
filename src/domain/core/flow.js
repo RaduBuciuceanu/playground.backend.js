cf258952573d0c5e163d309e92c31a451f91f097
radu <radu.buciuceanu@gmail.com>
2021-12-10 23:21:12 +0200

    [feature/37] Add try catch to flow

--- src/domain/core/flow.js
+++ src/domain/core/flow.js
@@ -1,7 +1,9 @@
 import Result from 'crocks/Result'
 import ReaderT from 'crocks/Reader/ReaderT'
+import tryCatch from 'crocks/Result/tryCatch'
 
 export const Flow = ReaderT(Result)
 
 Flow.Ok = Result.Ok
 Flow.Err = Result.Err
+Flow.tryCatch = tryCatch
