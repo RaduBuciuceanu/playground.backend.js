e822ee329bcdeeed53ddfc4028a72c8d4bb11e23
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-12-11 13:46:19 +0200

    [feature/47] Configure morgan logger (#48)
    * [feature/47] Reference morgan package
    
    * [feature/47] Use morgan middleware

--- src/presentation/server.js
+++ src/presentation/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const genericErrorHandler = (error, request, response, _next) => {
   response.status(500).json("Something wrong happened.")
@@ -8,6 +9,7 @@ const create = () => {
   const server = express()
   server.use(express.json())
   server.use(genericErrorHandler)
+  server.use(morgan('dev'))
 
   return server
 }
